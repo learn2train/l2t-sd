2b5ca579152280b1d3a04b250128c6e3b7cb2cf2
learn2train <admin@learn2train.org>
2023-08-11 18:30:09 +0100

    Take z axis type and values as well

--- utils/prompt2test.py
+++ utils/prompt2test.py
@@ -5,46 +5,52 @@ import json
 import argparse
 
 
-def main(filename, prompt, prompt_sr, seed):
+def main(output_file, prompt, negative_prompt, seed, z_axis_type, z_axis_values):
     """
-    Create or add Prompt S/R tests to a JSON file.
+    Create or add prompts to a XYZ grid prompt json file.
 
     Examples:
-    $ prompt2test.py -p 'A scene from 10 Cloverfield Lane' -r '10 Cloverfield Lane, nightmare on elm street, matrix, star wars, 20th century women, toy story'
-    $ prompt2test.py -p 'A photo of a cartoon character' -r 'cartoon character, monkey, dog, cat, statue, painting, pottery, car, house, city'
-    $ prompt2test.py -p 'A photo of Morgan Freeman' -r 'Morgan Freeman, Tom Cruise, Rihanna, Emma Watson, person, man, woman, boy, girl'
+    $ prompt2test.py -i prompts.txt -O xyz_prompts_test.json -z "CFG Scale" -Z "4,7,11" 
+    $ prompt2test.py -p 'A photo of a cartoon' --seed 1234 --z_axis_type "Prompt S/R" --z_axis_values="cartoon, monkey, dog, cat, statue, painting, pottery, car, house, city"
+    $ prompt2test.py -p 'A portrait of Morgan Freeman' -N "cartoon, 3D"
     """
-    # Open tests file
+    # Open json file (if exists)
     try:
-        with open (filename, 'r') as f:
+        with open (output_file, 'r') as f:
             data = json.loads(f.read())
     except Exception:
         data = []
-    # Add test to file
+    # Add prompt to json file
     data.extend([{
          'prompt': prompt,
-         'prompt_sr': prompt_sr,
+         'negative_prompt': negative_prompt,
          'seed': seed,
-         'z_axis_type': "Prompt S/R"
+         'z_axis_type': z_axis_type,
+         'z_axis_values': z_axis_values
+
     }])
     # Save prompts to JSON file
-    with open(filename, 'w') as fp:
+    with open(output_file, 'w') as fp:
         json.dump(data, fp)
 
 
 if __name__ == "__main__":
     # Parse args
     parser = argparse.ArgumentParser()
-    parser.add_argument('-f', '--filename', type=str, default='prompt_sr_tests.json', help='The name of the tests JSON file')
+    parser.add_argument('-O', '--output_file', type=str, default='xyz_prompts.json', help='The name of the JSON file (default: xyz_prompts.json)')
     parser.add_argument('-p', '--prompt', type=str, help='Prompt')
-    parser.add_argument('-r', '--prompt_sr', type=str, help='Prompt S/R')
-    parser.add_argument('--seed', type=int, default=555, help='Seed')
+    parser.add_argument('-N', '--negative_prompt', type=str, default='', help="Negative prompt (default: '(low quality, worst quality)')")
+    parser.add_argument('-s', '--seed', type=int, default=-1, help='Seed value (default: -1)')
+    parser.add_argument('-z', '--z_axis_type', type=str, default='Nothing', help="Z axis type. Options: 'Nothing', 'Prompt S/R', 'Steps', 'CFG Scale', 'Sampler', etc. (default: 'Nothing')")
+    parser.add_argument('-Z', '--z_axis_values', default='', type=str, help="Z axis values. (default: '')")
     args = parser.parse_args()
 
-    filename = args.filename
+    output_file = args.output_file
     prompt = args.prompt
-    prompt_sr = args.prompt_sr
+    negative_prompt = args.negative_prompt
     seed = args.seed
+    z_axis_type = args.z_axis_type
+    z_axis_values = args.z_axis_values
 
-    main(filename, prompt, prompt_sr, seed)
+    main(output_file, prompt, negative_prompt, seed, z_axis_type, z_axis_values)
 
